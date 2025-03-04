e9fdcd671def1df502827aae3e51b2f15528c763
Aman Malik <163856210+Aman017@users.noreply.github.com>
2025-03-04 21:50:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-//your JS code here. If required.
 document.getElementById('openModal').onclick = function() {
   document.getElementById('myModal').style.display = "block";
-}
+};
 
-// Close the modal when clicking outside of it or on the close button
+// Close modal when clicking outside of it
 window.onclick = function(event) {
   var modal = document.getElementById('myModal');
-  if (event.target == modal) {
+  console.log(event.target, modal); // Debugging
+  if (event.target === modal) {
     modal.style.display = "none";
   }
-}
+};
 
+// Close modal on clicking the close button
 document.querySelector('.close-modal').onclick = function() {
   document.getElementById('myModal').style.display = "none";
-}
+};
